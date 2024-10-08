55773ceacd306f50059c5cd95e3f0f0da0f0e7a0
Carlos Eduardo <eduardocarlosdias1@gmail.com>
2024-10-08 14:13:12 -0300

    concluindo criação de componentes

--- converte-convert/src/app/app.module.ts
+++ converte-convert/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { PrincipalComponent } from './principal/principal.component';
 import { ConverterComponent } from './converter/converter.component';
 import { InicioComponent } from './inicio/inicio.component';
